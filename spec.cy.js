deb52e5d1092c05fa983ffc54e2bf5da0fe341db
Silvia Prada Aprilia <silviaprada0904@gmail.com>
2022-11-15 18:01:48 +0700

    search

---
+++ spec.cy.js
@@ -0,0 +1,7 @@
+describe('empty spec', () => {
+  it('passes', () => {
+    cy.visit('https://shopee.co.id')
+    cy.get('input[class="shopee-searchbar-input__input"]').type('sepatu')
+    cy.get('button[class="btn btn-solid-primary btn--s btn--inline shopee-searchbar__search-button"]').click()
+  })
+})
